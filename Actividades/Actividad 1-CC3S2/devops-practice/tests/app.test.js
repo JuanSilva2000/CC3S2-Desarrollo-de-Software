5f1f365bf741a1b5fd3c52d8986daeaedd9805bf
Juan Silva <juan.silva.r@uni.pe>
2024-08-28 21:35:09 -0500

    CI (2) fix-3

--- Actividades/Actividad 1-CC3S2/devops-practice/tests/app.test.js
+++ Actividades/Actividad 1-CC3S2/devops-practice/tests/app.test.js
@@ -2,6 +2,17 @@ const request = require("supertest")
 const app = require("../src/app")
 
 describe('GET /', () => {
+    let server;
+
+    beforeAll(() => {
+        server = app.listen(3000)
+    })
+
+    afterAll(() => {
+        server.close()
+    })
+
+
     it("should return Hello World!", async () => {
         const res = await request(app).get("/");
         expect(res.statusCode).toEqual(200);
