62b0afdc13fb726fd8e24d04c8b7e2ed9aaf09a6
Juan Silva <juan.silva.r@uni.pe>
2024-10-23 16:57:29 -0500

    Avance actividad rgb AAA

---
+++ Actividades/Actividad RGB/tests/test_user_manager.py
@@ -0,0 +1,14 @@
+import pytest
+from user_manager import UserManager, UserAlreadyExistsError
+
+def test_agregar_usuario_exitoso():
+    # Arrange
+    manager = UserManager()
+    username = "kapu"
+    password = "securepassword"
+
+    # Act
+    manager.add_user(username, password)
+
+    # Assert
+    assert manager.user_exists(username)
